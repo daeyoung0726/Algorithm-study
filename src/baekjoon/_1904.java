65269bae6bd7abecbf3f6a69aec630215afba2ea
DaeYoung Park <pdy000726@naver.com>
2024-04-21 02:48:41 +0900

    baekjoon -1904

---
+++ src/baekjoon/_1904.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _1904 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(dp(n));
+    }
+
+    private static int dp(int n) {
+        int[] arr = new int[n];
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        arr[0] = 1;
+        arr[1] = 2;
+        for(int i = 2; i < n; i++) {
+            arr[i] = (arr[i-1] + arr[i-2]) % 15746;
+        }
+        return arr[n-1];
+    }
+}
