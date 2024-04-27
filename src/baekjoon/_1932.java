1dbe0c6f6032f89fd347217ceb3c90b1c13420b3
DaeYoung Park <pdy000726@naver.com>
2024-04-28 02:51:01 +0900

    beakjoon - 1932 수정

--- src/baekjoon/_1932.java
+++ src/baekjoon/_1932.java
@@ -3,37 +3,38 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class _1932 {
-    private static ArrayList<Integer>[] list;
-    private static int MAX = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        list = new ArrayList[n];
+        int[][] arr = new int[n][n];
+        int[][] dp = new int[n][n];
+        int k;
         StringTokenizer st;
-        int j;
         for(int i = 0; i < n; i++) {
+            k = 0;
             st = new StringTokenizer(br.readLine(), " ");
-            list[i] = new ArrayList<>(i+1);
-            while(st.hasMoreTokens())
-                list[i].add(Integer.parseInt(st.nextToken()));
-        }
-        dfs(list[0].get(0), n, 0, 0);
-        System.out.println(MAX);
-    }
-
-    private static void dfs(int sum, int n, int dep, int idx) {
-        if(n == dep+1) {
-            MAX = Math.max(sum, MAX);
-            return;
+            while(st.hasMoreTokens()) {
+                arr[i][k++] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        dfs(sum+list[dep+1].get(idx), n, dep+1, idx);
-        dfs(sum+list[dep+1].get(idx+1), n, dep+1, idx+1);
+        for(int i = 0; i < n; i++) {    // dp 마지막 행 값 저장.
+            dp[n-1][i] = arr[n-1][i];
+        }
+
+        // 마지막 이전부터 초기화 시작.
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + arr[i][j];
+            }
+        }
+
+        System.out.println(dp[0][0]);
+
     }
 }
--- src/homework/extensibleHashing/ExtensibleHashST.java
+++
@@ -1,2 +0,0 @@
-package homework.extensibleHashing;public class ExtensibleHashST {
-}
