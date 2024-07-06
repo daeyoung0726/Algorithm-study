eb81373dc44e6fb5b5119b050c07a96fc94c4cbe
DaeYoung Park <pdy000726@naver.com>
2024-07-06 21:47:39 +0900

    baekjoon - 16139

--- src/baekjoon/누적합/_16139.java
+++ src/baekjoon/누적합/_16139.java
@@ -8,27 +8,36 @@ import java.util.StringTokenizer;
 public class _16139 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s = br.readLine();
-
         int q = Integer.parseInt(br.readLine());
+
+        int n = s.length();
+        int[][] arr = new int[26][n + 1];
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            arr[c - 'a'][i + 1] = 1;
+        }
+
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                arr[i][j] += arr[i][j - 1];
+            }
+        }
+
         StringTokenizer str;
         StringBuilder sb = new StringBuilder();
-        int count;
-
         for (int i = 0; i < q; i++) {
-            count = 0;
             str = new StringTokenizer(br.readLine(), " ");
-            char x = str.nextToken().charAt(0);
+
+            char c = str.nextToken().charAt(0);
             int l = Integer.parseInt(str.nextToken());
             int r = Integer.parseInt(str.nextToken());
 
-            for (int j = l; j <= r; j++) {
-                if (x == s.charAt(j))
-                    count++;
-            }
-            sb.append(count).append('\n');
+            sb.append(arr[c - 'a'][r+1] - arr[c - 'a'][l]).append('\n');
         }
+
         System.out.println(sb);
     }
 }
