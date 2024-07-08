79acbc7c9c2baa47b5d186cd045fa4118f304c61
DaeYoung Park <pdy000726@naver.com>
2024-07-08 13:17:49 +0900

    baekjoon - 24444

--- src/baekjoon/DFS_BFS/_24444.java
+++ src/baekjoon/DFS_BFS/_24444.java
@@ -1,2 +1,73 @@
-package baekjoon.DFS_BFS;public class _24444 {
+package baekjoon.DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class _24444 {
+
+    private static ArrayList<ArrayList<Integer>> graphs = new ArrayList<>();
+    private static boolean[] visited;
+    private static int[] result;
+    private static int count = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(str.nextToken());
+        int M = Integer.parseInt(str.nextToken());
+        int R = Integer.parseInt(str.nextToken());
+
+        visited = new boolean[N+1];
+        result = new int[N+1];
+
+        for (int i = 0; i < N+1; i++) {
+            graphs.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(str.nextToken());
+            int v = Integer.parseInt(str.nextToken());
+
+            graphs.get(u).add(v);
+            graphs.get(v).add(u);
+        }
+
+        for (int i = 1; i < N+1; i++) {
+            Collections.sort(graphs.get(i));
+        }
+
+        bfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < N+1; i++) {
+            sb.append(result[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(v);
+        visited[v] = true;
+
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+
+            result[x] = count++;
+
+            for (int y: graphs.get(x)) {
+                if (!visited[y]) {
+                    visited[y] = true;
+                    queue.add(y);
+                }
+            }
+        }
+    }
 }
