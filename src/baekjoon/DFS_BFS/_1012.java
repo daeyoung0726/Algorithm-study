51f6a6092435ab3dabee51fa64ff440a8f779b05
DaeYoung Park <pdy000726@naver.com>
2024-07-10 16:18:01 +0900

    baekjoon - 1012

--- src/baekjoon/DFS_BFS/_1012.java
+++ src/baekjoon/DFS_BFS/_1012.java
@@ -1,2 +1,93 @@
-package baekjoon.DFS_BFS;public class _1012 {
+package baekjoon.DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class _1012 {
+
+    private static int[][] visited;
+    private static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer str;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+            int M = Integer.parseInt(str.nextToken());   // 가로
+            int N = Integer.parseInt(str.nextToken());   // 세로
+            int K = Integer.parseInt(str.nextToken());
+
+            int max = Math.max(M, N);
+
+            visited = new int[N][M];
+
+            for (int j = 0; j < K; j++) {
+                str = new StringTokenizer(br.readLine(), " ");
+                int u = Integer.parseInt(str.nextToken());
+                int v = Integer.parseInt(str.nextToken());
+                visited[v][u] = 1;
+            }
+
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (visited[j][k] == 1) {
+                        bfs(j, k, M, N);
+                        count++;
+                    }
+                }
+            }
+
+            sb.append(count).append('\n');
+            count = 0;
+        }
+        System.out.println(sb);
+    }
+
+    private static void bfs(int x, int y, int M, int N) {
+        Queue<Point1> queue = new LinkedList<>();
+
+        int[] dx = { -1, 1, 0, 0 };
+        int[] dy = { 0, 0, -1, 1 };
+
+        queue.add(new Point1(x, y));
+        visited[x][y] = 0;
+
+        while (!queue.isEmpty()) {
+            Point1 now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now.getX() + dx[i];
+                int ny = now.getY() + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M && visited[nx][ny] == 1) {
+                    visited[nx][ny] = 0;
+                    queue.add(new Point1(nx, ny));
+                }
+            }
+
+        }
+    }
+}
+
+class Point1 {
+    private int x;
+    private int y;
+
+    Point1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getX() {
+        return this.x;
+    }
+
+    int getY() {
+        return this.y;
+    }
 }
--- src/baekjoon/DFS_BFS/_1260.java
+++ src/baekjoon/DFS_BFS/_1260.java
@@ -1,2 +1,74 @@
-package baekjoon.DFS_BFS;public class _1260 {
+package baekjoon.DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class _1260 {
+
+    private static ArrayList<ArrayList<Integer>> graphs = new ArrayList<>();
+    private static boolean[] visitedDFS;
+    private static boolean[] visitedBFS;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(str.nextToken());
+        int m = Integer.parseInt(str.nextToken());
+        int r = Integer.parseInt(str.nextToken());
+
+        visitedDFS = new boolean[n+1];
+        visitedBFS = new boolean[n+1];
+        for (int i = 0; i < n+1; i++) {
+            graphs.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(str.nextToken());
+            int v = Integer.parseInt(str.nextToken());
+            graphs.get(u).add(v);
+            graphs.get(v).add(u);
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            Collections.sort(graphs.get(i));
+        }
+
+        dfs(r);
+        System.out.println();
+        bfs(r);
+    }
+
+    private static void dfs(int v) {
+        visitedDFS[v] = true;
+        System.out.print(v + " ");
+
+        for (int x: graphs.get(v)) {
+            if (!visitedDFS[x])
+                dfs(x);
+        }
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(v);
+        visitedBFS[v] = true;
+
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+
+            System.out.print(x + " ");
+            for (int y: graphs.get(x)) {
+                if (!visitedBFS[y]) {
+                    visitedBFS[y] = true;
+                    queue.add(y);
+                }
+            }
+        }
+    }
 }
