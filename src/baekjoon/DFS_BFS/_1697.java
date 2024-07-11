0033e21eaab25e0c2dd758720082e0b1807b26f3
DaeYoung Park <pdy000726@naver.com>
2024-07-11 12:38:02 +0900

    baekjoon - 1697

--- src/baekjoon/DFS_BFS/_1697.java
+++ src/baekjoon/DFS_BFS/_1697.java
@@ -1,2 +1,43 @@
-package baekjoon.DFS_BFS;public class _1697 {
+package baekjoon.DFS_BFS;
+
+import java.util.*;
+
+public class _1697 {
+
+    private static int[] visited;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        visited = new int[100001];
+
+        System.out.println(bfs(N, K));
+    }
+
+    private static int bfs(int N, int K) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+        visited[N] = 1;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            if (now == K) {
+                return visited[now] - 1;
+            }
+
+            int[] next = { now-1, now+1, now*2 };
+
+            for (int i = 0; i < 3; i++) {
+                if (next[i] >= 0 && next[i] <= 100000 && visited[next[i]] == 0) {
+                    queue.add(next[i]);
+                    visited[next[i]] = visited[now] + 1;
+                }
+            }
+
+        }
+        return -1;
+    }
 }
