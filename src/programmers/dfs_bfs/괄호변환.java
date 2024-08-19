8cb0cb988c56fe190d9d4d7e4723ac87eea7bf8a
DaeYoung Park <pdy000726@naver.com>
2024-08-20 03:09:52 +0900

    이코테 - 곱하기혹은더하기(그리디)

---
+++ src/programmers/dfs_bfs/괄호변환.java
@@ -0,0 +1,2 @@
+package programmers.dfs_bfs;public class 괄호변환 {
+}
--- src/이코테/그리디/곱하기혹은더하기.java
+++ src/이코테/그리디/곱하기혹은더하기.java
@@ -1,2 +1,22 @@
-package 이코테.그리디;public class 곱하기혹은더하기 {
+package 이코테.그리디;
+
+import java.util.Scanner;
+
+public class 곱하기혹은더하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int a = str.charAt(i) - '0';
+
+            if (result <= 0) {
+                result += a;
+            } else
+                result *= a;
+        }
+        System.out.println(result);
+    }
 }
