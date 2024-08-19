310a95b8eaae0e28dc190c897f7b7e656fb65a0e
DaeYoung Park <pdy000726@naver.com>
2024-08-19 15:53:32 +0900

    이코테 - 모험가길드(그리디)

--- src/이코테/그리디/모험가길드.java
+++ src/이코테/그리디/모험가길드.java
@@ -1,2 +1,42 @@
-package 이코테.그리디;public class 모험가길드 {
+package 이코테.그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 모험가길드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> lists = new ArrayList<>();
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            lists.add(Integer.parseInt(str.nextToken()));
+        }
+
+        Collections.sort(lists, Collections.reverseOrder());
+
+        int result = 0;
+        int size = n;
+        for (int i = 0; i < n; i++) {
+            int x = lists.get(i);
+
+            if (x <= size+1) {
+                result++;
+                size -= x;
+            }
+
+            if (size == 0)
+                break;
+        }
+
+        System.out.println(result);
+    }
 }
