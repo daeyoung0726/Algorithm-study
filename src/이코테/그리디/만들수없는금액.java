d701a26df93793214860bfd05d9f0ab2db7edb6c
DaeYoung Park <pdy000726@naver.com>
2024-08-19 15:53:15 +0900

    이코테 - 만들수없는금액(그리디)

--- src/이코테/그리디/만들수없는금액.java
+++ src/이코테/그리디/만들수없는금액.java
@@ -1,2 +1,32 @@
-package 이코테.그리디;public class 만들수없는금액 {
+package 이코테.그리디;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class 만들수없는금액 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        int target = 1;
+
+        for (int x: list) {
+            if (target < x)
+                break;
+            target += x;
+        }
+
+        System.out.println(target);
+    }
 }
