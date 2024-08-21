c4bba6338be5d6485eeeceacf68f8725d26cebd5
DaeYoung Park <pdy000726@naver.com>
2024-08-21 14:54:26 +0900

    이코테 -  문자열뒤집기(그리디)

--- src/이코테/그리디/문자열뒤집기.java
+++ src/이코테/그리디/문자열뒤집기.java
@@ -1,2 +1,38 @@
-package 이코테.그리디;public class 문자열뒤집기 {
+package 이코테.그리디;
+
+import java.util.Scanner;
+
+public class 문자열뒤집기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int zero = 0;
+        int one = 0;
+
+        int before = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int x = str.charAt(i) - '0';
+
+            if (i == 0) {
+                if (x == 0)
+                    zero++;
+                else
+                    one++;
+                before = x;
+            }
+
+            if (x != before) {
+                if (x == 0)
+                    zero++;
+                else
+                    one++;
+            }
+
+            before = x;
+        }
+
+        System.out.println(Math.min(zero, one));
+    }
 }
