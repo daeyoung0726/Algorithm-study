148786357609ad79cfabdd8a104e9bb4f8dbc5e5
DaeYoung Park <pdy000726@naver.com>
2024-09-04 00:55:16 +0900

    baekjoon - 1062

--- src/baekjoon/_1062.java
+++ src/baekjoon/_1062.java
@@ -7,70 +7,71 @@ import java.util.*;
 
 public class _1062 {
 
-    private static boolean[] useAlpha;
-    private static String[] str;
-    private static int max = 0;
+    private static boolean[] alpha;
+    private static String[] word;
+    private static int n;
+    private static int k;
+    private static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(str.nextToken());
+        k = Integer.parseInt(str.nextToken());
         k -= 5;
-
-        if (k < 0) {
+        if (k < 0) {    // 0은 빼줘야함. antatica는 가능하기에
             System.out.println(0);
             return;
         }
 
-        useAlpha = new boolean[26];
-        str = new String[n];
-
-        defalutUse();
+        alpha = new boolean[26];
+        plusDefaultAlpha();
+        word = new String[n];
 
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            s = s.substring(4, s.length() - 4);  // 접두사 'anta', 접미사 'tica'
-            str[i] = s;
+            word[i] = s.substring(4, s.length() - 4);
         }
 
-        dfs(0, 0, k);
+        dfs(0, 0);
 
         System.out.println(max);
     }
 
-    private static void defalutUse() {
-        useAlpha['a' - 'a'] = true;
-        useAlpha['c' - 'a'] = true;
-        useAlpha['i' - 'a'] = true;
-        useAlpha['n' - 'a'] = true;
-        useAlpha['t' - 'a'] = true;
+    private static void plusDefaultAlpha() {
+        alpha['a' - 'a'] = true;
+        alpha['c' - 'a'] = true;
+        alpha['i' - 'a'] = true;
+        alpha['n' - 'a'] = true;
+        alpha['t' - 'a'] = true;
     }
 
-    private static void dfs(int idx, int start, int depth) {
-        if (depth == 0) {
+    private static void dfs(int start, int depth) {
+
+        if (depth == k) {
             int count = 0;
-            for (String word : str) {
-                boolean canRead = true;
-                for (int i = 0; i < word.length(); i++) {
-                    if (!useAlpha[word.charAt(i) - 'a']) {
-                        canRead = false;
+            for (String w: word) {
+                boolean check = true;
+                for (int i = 0; i < w.length(); i++) {
+                    if (!alpha[w.charAt(i) - 'a']) {
+                        check = false;
                         break;
                     }
                 }
-                if (canRead) count++;
+                if (check)
+                    count++;
             }
             max = Math.max(max, count);
             return;
         }
 
+
         for (int i = start; i < 26; i++) {
-            if (!useAlpha[i]) {
-                useAlpha[i] = true;
-                dfs(idx + 1, i + 1, depth - 1);
-                useAlpha[i] = false;
+            if (!alpha[i]) {
+                alpha[i] = true;
+                dfs(i + 1, depth + 1);
+                alpha[i] = false;
             }
         }
     }
