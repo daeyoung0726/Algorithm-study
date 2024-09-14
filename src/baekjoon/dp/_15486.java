e11ba192b6e6ec8866b41e8bf8688d2c6a97d076
DaeYoung Park <pdy000726@naver.com>
2024-09-15 02:03:38 +0900

    baekjoon - 15486

--- src/baekjoon/dp/_15486.java
+++ src/baekjoon/dp/_15486.java
@@ -1,2 +1,38 @@
-package baekjoon.dp;public class _15486 {
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class _15486 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] days = new int[n+1];
+        int[] moneys = new int[n+1];
+
+        StringTokenizer str;
+        for (int i = 1; i <= n; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+            days[i] = Integer.parseInt(str.nextToken());
+            moneys[i] = Integer.parseInt(str.nextToken());
+        }
+
+        int[] dp = new int[n+2];
+
+        for (int i = 1; i <= n; i++) {
+            int day = days[i];
+            int money = moneys[i];
+
+            if (i + day <= n+1) {
+                dp[i + day] = Math.max(dp[i + day], dp[i] + money);
+            }
+            dp[i+1] = Math.max(dp[i], dp[i+1]);
+        }
+
+        System.out.println(dp[n+1]);
+    }
 }
