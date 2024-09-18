8d51792c88e7f85f1dfdbe60456fc1e6ad7c3602
DaeYoung Park <pdy000726@naver.com>
2024-09-18 16:11:06 +0900

    baekjoon - 2293

--- src/baekjoon/dp/_2293.java
+++ src/baekjoon/dp/_2293.java
@@ -1,2 +1,33 @@
-package baekjoon.dp;public class _2293 {
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class _2293 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(str.nextToken());
+        int k = Integer.parseInt(str.nextToken());
+
+        int[] coin = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k+1];
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = dp[j] + dp[j - coin[i]];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
 }
