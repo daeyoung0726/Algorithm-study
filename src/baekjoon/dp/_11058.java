0ef0cae300b5f40916f1f1db8541fd9ecac4a1ab
DaeYoung Park <pdy000726@naver.com>
2024-09-20 00:00:47 +0900

    baekjoon - 11058

--- src/baekjoon/dp/_11058.java
+++ src/baekjoon/dp/_11058.java
@@ -1,2 +1,25 @@
-package baekjoon.dp;public class _11058 {
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class _11058 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i-1] + 1;
+
+            if (i > 3) {
+                dp[i] = Math.max(dp[i], dp[i-3]*2);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
 }
