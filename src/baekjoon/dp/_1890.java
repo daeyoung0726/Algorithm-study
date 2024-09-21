f617001c61f5f741e7cdcd79642f2235c6d44f44
DaeYoung Park <pdy000726@naver.com>
2024-09-22 01:54:13 +0900

    baekjoon - 1890

--- src/baekjoon/dp/_1890.java
+++ src/baekjoon/dp/_1890.java
@@ -1,2 +1,44 @@
-package baekjoon.dp;public class _1890 {
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class _1890 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[n][n];
+
+        StringTokenizer str;
+
+        for (int i = 0; i < n; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(str.nextToken());
+            }
+        }
+
+        long[][] dp = new long[n][n];
+        dp[0][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] != 0) {
+                    int jump = map[i][j];
+
+                    if (i + jump < n)
+                        dp[i + jump][j] += dp[i][j];
+
+                    if (j + jump < n)
+                        dp[i][j + jump] += dp[i][j];
+                }
+            }
+        }
+
+        System.out.println(dp[n-1][n-1]);
+    }
 }
