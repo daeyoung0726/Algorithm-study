e6ae7bd042a7ddf702f6ba3f74ccd28d7dda979c
DaeYoung Park <pdy000726@naver.com>
2024-09-23 01:26:38 +0900

    baekjoon - 3568

--- src/baekjoon/구현/_3568.java
+++ src/baekjoon/구현/_3568.java
@@ -1,2 +1,48 @@
-package baekjoon.구현;public class _3568 {
+package baekjoon.구현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class _3568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        String[] splitStr = str.split(" ");
+        String prefix = splitStr[0];
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < splitStr.length; i++) {
+
+            sb.append(prefix + reverseStr(splitStr[i])).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    private static String reverseStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        int alphaIdx = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                alphaIdx = i;
+            } else
+                break;
+        }
+        for (int i = s.length() - 2; i > alphaIdx; i--) {
+            if (s.charAt(i) == ']') {
+                sb.append("[]");
+                i--;
+            } else
+                sb.append(s.charAt(i));
+        }
+
+        sb.append(" " + s.substring(0, alphaIdx+1) + ";");
+        return sb.toString();
+    }
 }
