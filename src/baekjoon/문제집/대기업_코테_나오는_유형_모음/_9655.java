d3d112d27d713ba2cba40060731d567df879f44b
DaeYoung Park <pdy000726@naver.com>
2024-09-24 01:32:05 +0900

    baekjoon - 9655

--- src/baekjoon/문제집/대기업_코테_나오는_유형_모음/_9655.java
+++ src/baekjoon/문제집/대기업_코테_나오는_유형_모음/_9655.java
@@ -1,2 +1,42 @@
-package baekjoon.문제집.대기업_코테_나오는_유형_모음;public class _9655 {
+package baekjoon.문제집.대기업_코테_나오는_유형_모음;
+
+import java.util.Scanner;
+
+public class _9655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String name = (n % 2 == 1) ? "SK" : "CY";
+
+        System.out.println(name);
+    }
 }
+/*
+public class _9655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] dp = new int[n+1];
+
+        if (n >= 1)
+            dp[1] = 1;
+        if (n >= 2)
+            dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int x = dp[i-1];
+
+            if (dp[i] == 0) {
+                dp[i] = (x == 1) ? 2 : 1;
+                if (i+2 <= n)
+                    dp[i+2] = (x == 1) ? 2 : 1;
+            }
+        }
+
+        String name = (dp[n] == 1) ? "SK" : "CY";
+        System.out.println(name);
+    }
+}*/
