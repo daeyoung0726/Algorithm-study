da0e137e3117aedac8f57533f511d393f110c2ec
DaeYoung Park <pdy000726@naver.com>
2024-09-25 01:19:34 +0900

    baekjoon 11723

--- src/baekjoon/문제집/대기업_코테_나오는_유형_모음/_11723.java
+++ src/baekjoon/문제집/대기업_코테_나오는_유형_모음/_11723.java
@@ -1,2 +1,85 @@
-package baekjoon.문제집.대기업_코테_나오는_유형_모음;public class _11723 {
+package baekjoon.문제집.대기업_코테_나오는_유형_모음;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class _11723 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        MySet mySet = new MySet();
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer str;
+        for (int i = 0; i < n; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+
+            String call = str.nextToken();
+            int num = 0;
+            if (str.hasMoreTokens())
+                num = Integer.parseInt(str.nextToken());
+
+            switch (call) {
+                case "add": mySet.add(num);
+                    break;
+
+                case "remove": mySet.remove(num);
+                    break;
+
+                case "check": sb.append(mySet.check(num)).append('\n');
+                    break;
+
+                case "toggle": mySet.toggle(num);
+                    break;
+
+                case "all": mySet.all();
+                    break;
+
+                case "empty": mySet.empty();
+                    break;
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
+
+class MySet {
+    private int[] set;
+
+    MySet() {
+        this.set = new int[21];
+    }
+
+    public void add(int idx) {
+        set[idx - 1] = 1;
+    }
+
+    public void remove(int idx) {
+        set[idx - 1] = 0;
+    }
+
+    public int check(int idx) {
+        return set[idx - 1];
+    }
+
+    public void toggle(int idx) {
+        set[idx - 1] = (set[idx - 1] == 0) ? 1 : 0;
+    }
+
+    public void all() {
+        for (int i = 0; i < 20; i++) {
+            set[i] = 1;
+        }
+    }
+
+    public void empty() {
+        for (int i = 0; i < 20; i++) {
+            set[i] = 0;
+        }
+    }
 }
---
+++ src/programmers/고득점kit/DFS_BFS/네트워크.java
@@ -0,0 +1,2 @@
+package programmers.고득점kit.DFS_BFS;public class 네트워크 {
+}
