691a1b1534876f5a2a87f4fde53b528e93e690c5
DaeYoung Park <pdy000726@naver.com>
2024-09-26 00:48:56 +0900

    baekjoon - 1715

--- src/baekjoon/정렬/_1715.java
+++ src/baekjoon/정렬/_1715.java
@@ -1,2 +1,36 @@
-package baekjoon.정렬;public class _1715 {
+package baekjoon.정렬;
+
+import java.util.*;
+
+public class _1715 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            pq.add(sc.nextInt());
+        }
+
+        int cum = 0;
+        while (true) {
+            int a = 0;
+            int b = 0;
+            if (!pq.isEmpty())
+                a = pq.poll();
+            if (!pq.isEmpty())
+                b = pq.poll();
+            else {
+                break;
+            }
+
+            int sum = a + b;
+            cum += sum;
+            pq.add(sum);
+        }
+
+        System.out.println(cum);
+    }
 }
