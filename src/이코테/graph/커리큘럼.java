82c610120e674e04f3938a80811257a59cecd747
DaeYoung0726 <pdy000726@naver.com>
2024-09-29 12:01:13 +0900

    이코테(graph) - 커리큘럼

--- src/이코테/graph/커리큘럼.java
+++ src/이코테/graph/커리큘럼.java
@@ -3,9 +3,83 @@ package 이코테.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 커리큘럼 {
+
+    private static ArrayList<ArrayList<Integer>> graphs = new ArrayList<>();
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] inDegree = new int[n+1];
+        int[] time = new int[n+1];
+
+        for (int i = 0; i < n+1; i++) {
+            graphs.add(new ArrayList<>());
+        }
+
+        StringTokenizer str;
+
+        for (int i = 0; i < n; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+            time[i+1] = Integer.parseInt(str.nextToken());
+
+            while (str.hasMoreTokens()) {
+                int x = Integer.parseInt(str.nextToken());
+
+                if (x == -1)
+                    break;
+
+                inDegree[i+1]++;
+                graphs.get(x).add(i+1);
+            }
+        }
+
+        topologySort(inDegree, time, n);
+        System.out.println(sb);
+    }
+
+    private static void topologySort(int[] inDegree, int[] time, int n) {
+
+        int[] result = new int[n+1];
+
+        for (int i = 0; i < n; i++) {
+            result[i+1] = time[i+1];
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < n+1; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for (int x: graphs.get(now)) {
+                result[x] = Math.max(result[x], result[now] + time[x]);
+                inDegree[x]--;
+
+                if (inDegree[x] == 0)
+                    queue.add(x);
+            }
+        }
+
+        for (int i = 1; i < result.length; i++) {
+            sb.append(result[i]).append('\n');
+        }
+    }
+}
+
+/*public class 커리큘럼 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -37,7 +111,7 @@ public class 커리큘럼 {
 
         System.out.println(sb);
     }
-}
+}*/
 
 /* 아래 코드는 unionfind 사용할 시, 연관된거 못찾음. 위도 그렇겠지만.
 public class 커리큘럼 {
