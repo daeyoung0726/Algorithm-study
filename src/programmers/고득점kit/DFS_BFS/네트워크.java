cc5d39110714d3f1afc2871cb9dfc2227dc9eb2f
DaeYoung0726 <pdy000726@naver.com>
2024-10-07 16:31:30 +0900

    programmers - 네트워크

--- src/programmers/고득점kit/DFS_BFS/네트워크.java
+++ src/programmers/고득점kit/DFS_BFS/네트워크.java
@@ -1,2 +1,55 @@
-package programmers.고득점kit.DFS_BFS;public class 네트워크 {
+package programmers.고득점kit.DFS_BFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class 네트워크 {
+
+    private ArrayList<ArrayList<Integer>> graphs = new ArrayList<>();
+    private boolean[] visited;
+
+    public int solution(int n, int[][] computers) {
+
+        visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            graphs.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    graphs.get(i).add(j);
+                    graphs.get(j).add(i);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(i);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private void bfs(int x) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(x);
+        visited[x] = true;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for (int val: graphs.get(now)) {
+                if (!visited[val]) {
+                    visited[val] = true;
+                    queue.add(val);
+                }
+            }
+        }
+    }
 }
