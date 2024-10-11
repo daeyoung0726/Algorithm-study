308961513436b4a20730691e7e34f5125defa386
DaeYoung0726 <pdy000726@naver.com>
2024-10-11 12:27:54 +0900

    programmers - 등굣길

--- src/programmers/고득점kit/dp/등굣길.java
+++ src/programmers/고득점kit/dp/등굣길.java
@@ -1,2 +1,34 @@
-package programmers.고득점kit.dp;public class 등굣길 {
+package programmers.고득점kit.dp;
+
+class 등굣길 {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+
+        int[][] dp = new int[n][m];
+        boolean[][] notAccess = new boolean[n][m];
+
+        for (int i = 0; i < puddles.length; i++) {
+            int x = puddles[i][0] - 1;
+            int y = puddles[i][1] - 1;
+            notAccess[y][x] = true;
+        }
+
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (!notAccess[i][j]) {
+                    if (i - 1 >= 0 && !notAccess[i-1][j]) {
+                        dp[i][j] += dp[i - 1][j] % 1000000007;
+                    }
+                    if (j - 1 >= 0 && !notAccess[i][j-1]) {
+                        dp[i][j] += dp[i][j-1] % 1000000007;
+                    }
+                }
+            }
+        }
+
+        return dp[n-1][m-1] % 1000000007;
+    }
 }
