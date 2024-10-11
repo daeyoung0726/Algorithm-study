52d00aca31abd62e1ae76cf99e75b08b32ca50fb
DaeYoung0726 <pdy000726@naver.com>
2024-10-11 12:27:27 +0900

    programmers - 타겟 넘버

--- src/programmers/고득점kit/DFS_BFS/타겟_넘버.java
+++ src/programmers/고득점kit/DFS_BFS/타겟_넘버.java
@@ -1,2 +1,57 @@
-package programmers.고득점kit.DFS_BFS;public class 타겟_넘버 {
+package programmers.고득점kit.DFS_BFS;
+
+import java.util.*;
+
+class 타겟_넘버 {
+
+    private int len;
+    private int[] n;
+    private int count = 0;
+
+    public int solution(int[] numbers, int target) {
+
+        len = numbers.length;
+        n = numbers;
+        dfs(0, 0, target);
+        //return bfs(target);
+        return count;
+    }
+
+    private void dfs(int idx, int sum, int target) {
+        if (idx == len) {
+            if (sum == target) {
+                count++;
+            }
+            return;
+        }
+
+        dfs(idx + 1, sum + n[idx], target);
+        dfs(idx + 1, sum - n[idx], target);
+    }
+
+    private int bfs(int target) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(new int[] {n[0], 1});
+        queue.add(new int[] {-n[0], 1});
+
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int num = now[0];
+            int depth = now[1];
+
+            if (depth == len) {
+                if (num == target) {
+                    count++;
+                }
+                continue;
+            }
+
+            queue.add(new int[] {num + n[depth], depth + 1});
+            queue.add(new int[] {num - n[depth], depth + 1});
+        }
+
+        return count;
+    }
 }
