9f50472b7344bcd00d2afb1967ac4e9dd867fc44
DaeYoung0726 <pdy000726@naver.com>
2024-10-13 01:56:29 +0900

    programmers - 단속카메라

--- src/programmers/고득점kit/그리디/단속카메라.java
+++ src/programmers/고득점kit/그리디/단속카메라.java
@@ -1,2 +1,22 @@
-package programmers.고득점kit.그리디;public class 단속카메라 {
+package programmers.고득점kit.그리디;
+
+import java.util.*;
+
+class 단속카메라 {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (a, b) -> a[0] - b[0]);
+
+        int answer = 1;
+        int lastCamera = routes[0][1];
+
+        for (int i = 1; i < routes.length; i++) {
+            if (lastCamera < routes[i][0]) {
+                answer++;
+                lastCamera = routes[i][1];
+            } else
+                lastCamera = Math.min(lastCamera, routes[i][1]);
+        }
+
+        return answer;
+    }
 }
