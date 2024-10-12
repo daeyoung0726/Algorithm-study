20629ee027b11b5a50b503301a85c6e68eb94d08
DaeYoung0726 <pdy000726@naver.com>
2024-10-13 01:56:17 +0900

    programmers - 구명보트

--- src/programmers/고득점kit/그리디/구명보트.java
+++ src/programmers/고득점kit/그리디/구명보트.java
@@ -1,2 +1,29 @@
-package programmers.고득점kit.그리디;public class 구명보트 {
+package programmers.고득점kit.그리디;
+
+import java.util.Arrays;
+
+class 구명보트 {
+    public int solution(int[] people, int limit) {
+
+        Arrays.sort(people);
+        int start = 0;
+        int end = people.length - 1;
+        int answer = 0;
+
+        while (start <= end) {
+            if (start == end) {
+                answer++;
+                end--;
+                continue;
+            }
+
+            if (people[end] + people[start] <= limit) {
+                start++;
+            }
+            end--;
+            answer++;
+        }
+
+        return answer;
+    }
 }
