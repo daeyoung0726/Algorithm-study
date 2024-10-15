69b47371eb0e065f237768369f2d03690fb2ee7a
DaeYoung0726 <pdy000726@naver.com>
2024-10-16 02:13:41 +0900

    programmers - 같은 숫자는 싫어

--- src/programmers/고득점kit/스택_큐/같은_숫자는_싫어.java
+++ src/programmers/고득점kit/스택_큐/같은_숫자는_싫어.java
@@ -1,2 +1,25 @@
-package programmers.고득점kit.스택_큐;public class 같은_숫자는_싫어 {
+package programmers.고득점kit.스택_큐;
+
+import java.util.*;
+
+class 같은_숫자는_싫어 {
+    public int[] solution(int[] arr) {
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (deque.isEmpty()) {
+                deque.add(arr[i]);
+            } else {
+                if (deque.peekLast() != arr[i])
+                    deque.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[deque.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = deque.poll();
+        }
+
+        return answer;
+    }
 }
