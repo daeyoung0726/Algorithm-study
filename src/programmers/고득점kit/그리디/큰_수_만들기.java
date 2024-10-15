eb5cd0f4bdc4aaff5a2e9bf7e1025facfe445ee5
DaeYoung0726 <pdy000726@naver.com>
2024-10-16 02:13:20 +0900

    programmers - 큰 수 만들기

--- src/programmers/고득점kit/그리디/큰_수_만들기.java
+++ src/programmers/고득점kit/그리디/큰_수_만들기.java
@@ -1,2 +1,34 @@
-package programmers.고득점kit.그리디;public class 큰_수_만들기 {
+package programmers.고득점kit.그리디;
+
+import java.util.Stack;
+
+class 큰_수_만들기 {
+    public String solution(String number, int k) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            int num = number.charAt(i) - '0';
+
+            while (k > 0 && !stack.isEmpty() && stack.peek() < num) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num);
+        }
+
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int size = stack.size();
+
+        for (int i = 0; i < size; i++) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
 }
