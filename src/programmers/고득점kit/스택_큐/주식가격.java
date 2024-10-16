49a88c15358919242d0626325febbf160eb72745
DaeYoung0726 <pdy000726@naver.com>
2024-10-17 01:17:06 +0900

    programmers - 주식가격

--- src/programmers/고득점kit/스택_큐/주식가격.java
+++ src/programmers/고득점kit/스택_큐/주식가격.java
@@ -1,2 +1,42 @@
-package programmers.고득점kit.스택_큐;public class 주식가격 {
+package programmers.고득점kit.스택_큐;
+
+import java.util.*;
+
+class 주식가격 {
+    public int[] solution(int[] prices) {
+        Stack<Point> stack = new Stack<>();
+
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(new Point(i, prices[i]));
+            }
+
+            while (true) {
+                if (!stack.isEmpty() && stack.peek().price > prices[i]) {
+                    Point p = stack.pop();
+                    answer[p.idx] = i - p.idx;
+                } else if (!stack.isEmpty() && i == prices.length - 1) {    // 마지막
+                    Point p = stack.pop();
+                    answer[p.idx] = i - p.idx;
+                } else {
+                    stack.push(new Point(i, prices[i]));
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
+class Point {
+    int idx;
+    int price;
+
+    Point(int idx, int price) {
+        this.idx = idx;
+        this.price = price;
+    }
 }
