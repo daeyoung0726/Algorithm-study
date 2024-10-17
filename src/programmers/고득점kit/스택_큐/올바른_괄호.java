d874738c95ce2c205835846e4ec613fe2a3932b0
DaeYoung0726 <pdy000726@naver.com>
2024-10-18 01:05:21 +0900

    programmers - 올바른 괄호

--- src/programmers/고득점kit/스택_큐/올바른_괄호.java
+++ src/programmers/고득점kit/스택_큐/올바른_괄호.java
@@ -1,2 +1,28 @@
-package programmers.고득점kit.스택_큐;public class 올바른_괄호 {
+package programmers.고득점kit.스택_큐;
+
+import java.util.*;
+
+class 올바른_괄호 {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        boolean answer = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    answer = false;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        if (!stack.isEmpty())
+            return false;
+        return answer;
+    }
 }
