ef7405dbfc5d4a1e6e46927c4262270f0ec5655d
DaeYoung0726 <pdy000726@naver.com>
2024-10-19 03:00:49 +0900

    programmers - 프로세스

--- src/programmers/고득점kit/스택_큐/프로세스.java
+++ src/programmers/고득점kit/스택_큐/프로세스.java
@@ -1,2 +1,44 @@
-package programmers.고득점kit.스택_큐;public class 프로새스 {
+package programmers.고득점kit.스택_큐;
+
+import java.util.*;
+
+class 프로세스 {
+    public int solution(int[] priorities, int location) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(priorities[i]);
+        }
+
+        int answer = 0;
+        int count = 1;
+        boolean priorityCheck;
+        while (true) {
+            int now = queue.poll();
+            priorityCheck = true;
+            for (int x: queue) {
+                if (now < x) {
+                    priorityCheck = false;
+                    break;
+                }
+            }
+
+            if (!priorityCheck) {
+                queue.add(now);
+            } else {
+                if (location == 0) {
+                    answer = count;
+                    break;
+                } else {
+                    count++;
+                }
+            }
+
+            location = (location == 0) ? queue.size() - 1 : location - 1;
+        }
+
+
+        return answer;
+    }
 }
