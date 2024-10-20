b898acd266a4aa357d03380da0d627878e1d6b64
DaeYoung0726 <pdy000726@naver.com>
2024-10-21 00:19:33 +0900

    programmers - 최조 직사각형

--- src/programmers/고득점kit/완전탐색/최소직사각형.java
+++ src/programmers/고득점kit/완전탐색/최소직사각형.java
@@ -1,2 +1,22 @@
-package programmers.고득점kit.완전탐색;public class 최소직사각형 {
+package programmers.고득점kit.완전탐색;
+
+class 최소직사각형 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] > sizes[i][1]) {
+                max = Math.max(max, sizes[i][0]);
+                min = Math.max(min, sizes[i][1]);
+            } else {
+                max = Math.max(max, sizes[i][1]);
+                min = Math.max(min, sizes[i][0]);
+            }
+        }
+
+        answer = max * min;
+        return answer;
+    }
 }
