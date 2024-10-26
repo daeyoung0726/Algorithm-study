e5cba73d7799785e4c0f176048ba83e610b257c9
DaeYoung0726 <pdy000726@naver.com>
2024-10-27 02:50:06 +0900

    programmers - 더 맵게

--- src/programmers/고득점kit/힙/더_맵게.java
+++ src/programmers/고득점kit/힙/더_맵게.java
@@ -1,2 +1,32 @@
-package programmers.고득점kit.힙;public class 더_맵게 {
+package programmers.고득점kit.힙;
+
+import java.util.PriorityQueue;
+
+class 더_맵게 {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+
+        int answer = 0;
+
+        while (true) {
+            if (pq.peek() < K) {
+                if (pq.size() < 2) {
+                    answer = -1;
+                    break;
+                }
+                int x = pq.poll();
+                int y = pq.poll();
+                int sum = x + 2*y;
+                pq.add(sum);
+                answer++;
+            } else
+                break;
+        }
+
+        return answer;
+    }
 }
