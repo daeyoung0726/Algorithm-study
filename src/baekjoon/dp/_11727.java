cec0dde8049e9e90aca9e78878aadc865a34a356
DaeYoung0726 <pdy000726@naver.com>
2024-11-06 01:03:32 +0900

    baekjoon - 11727

--- src/baekjoon/dp/_11727.java
+++ src/baekjoon/dp/_11727.java
@@ -1,2 +1,22 @@
-package baekjoon.dp;public class _11727 {
+package baekjoon.dp;
+
+import java.util.Scanner;
+
+public class _11727 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;      // https://dev01.tistory.com/70
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
 }
