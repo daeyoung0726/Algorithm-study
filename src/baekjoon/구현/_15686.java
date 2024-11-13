f8dec9ee662485a9f9b4635afd65fc77b920552f
DaeYoung0726 <pdy000726@naver.com>
2024-11-13 17:49:39 +0900

    baekjoon - 15686

--- src/baekjoon/구현/_15686.java
+++ src/baekjoon/구현/_15686.java
@@ -1,5 +1,9 @@
 package baekjoon.구현;
 
+// 집과 치킨에 대해서 구분을 함. (list 사용)
+// 브루트포스를 통해서 치킨집에 대한 경우의 수 다 구함.
+// 치킨집 개수 다 채워지면 계산
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -7,87 +11,87 @@ import java.util.*;
 
 public class _15686 {
 
-    private static ArrayList<Point1> houses = new ArrayList<>();
-    private static ArrayList<Point1> chickens = new ArrayList<>();
-    private static int n;
-    private static int m;
-    private static int min = Integer.MAX_VALUE;
     private static boolean[] visited;
+    private static ArrayList<int[]> homes;
+    private static ArrayList<int[]> chickens;
+
+    private static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer str = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(str.nextToken());
-        m = Integer.parseInt(str.nextToken());
+
+        int n = Integer.parseInt(str.nextToken());
+        int m = Integer.parseInt(str.nextToken());
+
+        homes = new ArrayList<>();
+        chickens = new ArrayList<>();
 
         int[][] map = new int[n][n];
+
         for (int i = 0; i < n; i++) {
             str = new StringTokenizer(br.readLine(), " ");
-            int j = 0;
-            while(str.hasMoreTokens()) {
-                map[i][j++] = Integer.parseInt(str.nextToken());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(str.nextToken());
             }
         }
 
-        findHouseAndChicken(map);
+        findHomeAndChicken(map, n);
         visited = new boolean[chickens.size()];
-
-        dfs(0, 0);
+        dfs(m, 0, 0);
 
         System.out.println(min);
     }
 
-    private static void findHouseAndChicken(int[][] map) {
-
+    private static void findHomeAndChicken(int[][] map, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (map[i][j] == 1) {
-                    houses.add(new Point1(i, j));
+                    homes.add(new int[] {i, j});
                 }
                 if (map[i][j] == 2) {
-                    chickens.add(new Point1(i, j));
+                    chickens.add(new int[] {i, j});
                 }
             }
         }
     }
 
-    private static void dfs(int start, int cnt) {
-        if (cnt == m) {
-            int total = 0;
+    private static void dfs(int m, int x, int depth) {
+        if (m == depth) {
+            int sum;
+            int result = 0;
+            for (int i = 0; i < homes.size(); i++) {
+                int[] nowHome = homes.get(i);
+                int homeX = nowHome[0];
+                int homeY = nowHome[1];
+                sum = Integer.MAX_VALUE;
 
-            for (int i = 0; i < houses.size(); i++) {
-                int sum = Integer.MAX_VALUE;
-                Point1 house = houses.get(i);
                 for (int j = 0; j < chickens.size(); j++) {
                     if (visited[j]) {
-                        Point1 chicken = chickens.get(j);
-                        sum = Math.min(sum,
-                                Math.abs(house.x - chicken.x) + Math.abs(house.y - chicken.y));
+                        int[] nowChicken = chickens.get(j);
+                        int chickenX = nowChicken[0];
+                        int chickenY = nowChicken[1];
+
+                        sum = Math.min(sum, calc(homeX, homeY, chickenX, chickenY));
                     }
                 }
-                total += sum;
+                result += sum;
             }
-            min = Math.min(min, total);
+            min = Math.min(min, result);
             return;
         }
 
-        for (int i = start; i < chickens.size(); i++) {
+        for (int i = x; i < chickens.size(); i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                dfs(i+1, cnt+1);
+                dfs(m, i + 1, depth + 1);
                 visited[i] = false;
             }
         }
     }
-}
 
-class Point1 {
-    int x;
-    int y;
-
-    Point1(int x, int y) {
-        this.x = x;
-        this.y = y;
+    private static int calc(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 }
---
+++ src/programmers/카카오_2024/가장_많이_받은_선물.java
@@ -0,0 +1,2 @@
+package programmers.카카오_2024;public class 가장_많이_받은_선물 {
+}
