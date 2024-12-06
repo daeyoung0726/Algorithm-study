e09a6887274ed6744f4ca495c6cc0eca1988e251
DaeYoung0726 <pdy000726@naver.com>
2024-12-06 20:44:39 +0900

    programmers - 거스름돈

--- src/programmers/dp/거스름돈.java
+++ src/programmers/dp/거스름돈.java
@@ -1,2 +1,20 @@
-package programmers.dp;public class 거스름돈 {
+package programmers.dp;
+
+import java.util.Arrays;
+
+class 거스름돈 {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+
+        Arrays.sort(money);
+
+        dp[0] = 1;
+
+        for (int i = 0; i < money.length; i++) {
+            for (int j = money[i]; j <= n; j++) {
+                dp[j] += dp[j - money[i]];
+            }
+        }
+        return dp[n];
+    }
 }
