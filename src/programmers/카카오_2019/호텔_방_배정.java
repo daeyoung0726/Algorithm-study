3e743d9717eae7f484166f60a22ece8f5e757dc9
DaeYoung0726 <pdy000726@naver.com>
2024-12-09 17:10:27 +0900

    programmers - 호텔 방 배정

--- src/programmers/카카오_2019/호텔_방_배정.java
+++ src/programmers/카카오_2019/호텔_방_배정.java
@@ -1,2 +1,30 @@
-package programmers.카카오_2019;public class 호텔_방_배정 {
+package programmers.카카오_2019;
+
+import java.util.*;
+
+public class 호텔_방_배정 {
+    private Map<Long, Long> map;
+
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+
+        map = new HashMap<>();
+
+        for (int i = 0; i < room_number.length; i++) {
+            answer[i] = findNum(room_number[i]);
+        }
+        return answer;
+    }
+
+    private long findNum(long roomNum) {
+        if (!map.containsKey(roomNum)) {
+            map.put(roomNum, roomNum + 1);
+            return roomNum;
+        }
+
+        long nextRoomNum = findNum(map.get(roomNum));
+        map.put(roomNum, nextRoomNum + 1);
+
+        return nextRoomNum;
+    }
 }
