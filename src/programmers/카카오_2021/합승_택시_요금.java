7f7e548c29efd81c02b8bd5c17737c82b22f8e81
DaeYoung0726 <pdy000726@naver.com>
2024-12-15 17:05:24 +0900

    programmers - 합승 택시 요금

--- src/programmers/카카오_2021/합승_택시_요금.java
+++ src/programmers/카카오_2021/합승_택시_요금.java
@@ -1,2 +1,68 @@
-package programmers.카카오_2021;public class 합승_택시_요금 {
+package programmers.카카오_2021;
+
+// 하나의 지점 정한 후, 그 지점에서 다익스트라를 하여 a,b를 찾는방식
+
+import java.util.*;
+
+public class 합승_택시_요금 {
+
+    private ArrayList<ArrayList<int[]>> graphs = new ArrayList<>();
+
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+
+        for (int i = 0; i <= n; i++) {
+            graphs.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < fares.length; i++) {
+            int x = fares[i][0];
+            int y = fares[i][1];
+            int dis = fares[i][2];
+
+            graphs.get(x).add(new int[] {y, dis});
+            graphs.get(y).add(new int[] {x, dis});
+        }
+
+        int[] distanceS = dijkstra(s, n);
+        int[] distanceA = dijkstra(a, n);
+        int[] distanceB = dijkstra(b, n);
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            min = Math.min(min, distanceS[i] + distanceA[i] + distanceB[i]);
+        }
+
+        return min;
+    }
+
+    private int[] dijkstra(int start, int n) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> x[1] - y[1]);
+
+        int[] distance = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            distance[i] = Integer.MAX_VALUE;
+        }
+
+        distance[start] = 0;
+        pq.add(new int[] {start, 0});
+
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+
+            int nn = now[0];
+            int dis = now[1];
+            if (distance[nn] < dis)
+                continue;
+            for (int[] next: graphs.get(nn)) {
+                if (distance[next[0]] > distance[nn] + next[1]) {
+                    distance[next[0]] = distance[nn] + next[1];
+                    pq.add(new int[] {next[0], distance[next[0]]});
+                }
+            }
+        }
+
+        return distance;
+    }
 }
