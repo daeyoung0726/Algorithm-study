902ec3d3a45f0c2c4d253778050a70271219dae2
DaeYoung0726 <pdy000726@naver.com>
2024-12-16 17:51:03 +0900

    programmers - k진수에 소수 개수 구하기

--- src/programmers/카카오_2022/k진수에_소수_개수_구하기.java
+++ src/programmers/카카오_2022/k진수에_소수_개수_구하기.java
@@ -1,2 +1,62 @@
-package programmers.카카오_2022;public class k진수에_소수_개수_구하기 {
+package programmers.카카오_2022;
+
+public class k진수에_소수_개수_구하기 {
+
+    public int solution(int n, int k) {
+
+        String trans = Integer.toString(n, k);
+
+        return findNum(trans);
+    }
+
+    private int findNum(String trans) {
+
+        int startIdx = 0;
+        int count = 0;
+        int answer = 0;
+        for (int i = 0; i < trans.length(); i++) {
+            if (trans.charAt(i) != '0') {
+                if (count == 0) {
+                    startIdx = i;
+                }
+                count++;
+            } else {
+                if (count != 0) {
+                    long num = Long.parseLong(
+                            trans.substring(startIdx, startIdx + count)
+                    );
+                    if (isPrime(Long.toString(num, 10))) {
+                        answer++;
+                    }
+                }
+                count = 0;
+            }
+        }
+
+        if (count != 0) {
+            long num = Long.parseLong(
+                    trans.substring(startIdx, startIdx + count)
+            );
+            if (isPrime(Long.toString(num, 10))) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isPrime(String n) {
+        Long num = Long.parseLong(n);
+        if (num <= 1)
+            return false;
+
+
+        for (long i = 2; i * i <= num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
 }
