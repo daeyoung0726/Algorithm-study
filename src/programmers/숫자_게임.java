cdfbde966acbeec2e28e66be927ba4d0920564e4
DaeYoung0726 <pdy000726@naver.com>
2025-01-14 11:23:15 +0900

    programmers - 숫자 게임

--- src/programmers/숫자_게임.java
+++ src/programmers/숫자_게임.java
@@ -1,2 +1,26 @@
-package programmers;public class 숫자_게임 {
+package programmers;
+
+// a, b 모두 정렬 후 a의 값보다 큰 값
+
+import java.util.*;
+
+public class 숫자_게임 {
+    public int solution(int[] A, int[] B) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int idx = 0;
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            while (idx < B.length) {
+                if (B[idx++] > A[i]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
 }
