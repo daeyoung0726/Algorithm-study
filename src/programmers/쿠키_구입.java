fdc7a44322568bb9a5fccd1409bef7f1a75e11f0
DaeYoung0726 <pdy000726@naver.com>
2025-01-23 18:28:13 +0900

    programmers - 쿠키 구입

--- src/programmers/쿠키_구입.java
+++ src/programmers/쿠키_구입.java
@@ -1,2 +1,28 @@
-package programmers;public class 쿠키_구입 {
+package programmers;
+
+public class 쿠키_구입 {
+    public int solution(int[] cookie) {
+        int n = cookie.length;
+        int max = 0;
+
+        for (int m = 0; m < n - 1; m++) {
+            int left = m;
+            int right = m + 1;
+            int leftSum = cookie[left];
+            int rightSum = cookie[right];
+
+            while (left >= 0 && right < n) {
+                if (leftSum == rightSum)
+                    max = Math.max(max, leftSum);
+
+                if (leftSum <= rightSum && --left >= 0) {
+                    leftSum += cookie[left];
+                } else if (leftSum > rightSum && ++right < n) {
+                    rightSum += cookie[right];
+                }
+            }
+        }
+
+        return max;
+    }
 }
