274c14a232342688b84f1a18d9ef3047d0dba92f
DaeYoung0726 <pdy000726@naver.com>
2025-03-15 14:55:51 +0900

    programmers - 가장 긴 팰린드롬

--- src/programmers/가장_긴_팰린드롬.java
+++ src/programmers/가장_긴_팰린드롬.java
@@ -1,2 +1,29 @@
-package programmers;public class 가장_긴_팰린드롬 {
+package programmers;
+
+public class 가장_긴_팰린드롬 {
+    public int solution(String s) {
+        int answer = 1;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            int temp1 = run(i, i, s);
+            int temp2 = run(i, i + 1, s);
+            int temp = Math.max(temp1, temp2);
+            answer = Math.max(answer, temp);
+        }
+
+        return answer;
+    }
+
+    private int run(int left, int right, String s) {
+        while (left >= 0 && right < s.length()) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+
+            left--;
+            right++;
+        }
+
+        return right - left - 1;    // left, right는 이미 증감연산자로 넘어간 후임.
+    }
 }
