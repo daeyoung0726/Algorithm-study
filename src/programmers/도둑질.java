e5a04c64c2a4465bad315029bcc992cb04efa16f
DaeYoung0726 <pdy000726@naver.com>
2025-03-16 22:02:06 +0900

    programmers - 도둑질

--- src/programmers/도둑질.java
+++ src/programmers/도둑질.java
@@ -1,2 +1,26 @@
-package programmers;public class 도둑질 {
+package programmers;
+
+public class 도둑질 {
+    public int solution(int[] money) {
+        int len = money.length;
+
+        int[] dp1 = new int[len];
+        int[] dp2 = new int[len];
+
+        // 첫번째 집 선택하는 경우
+        dp1[0] = money[0];
+        dp1[1] = money[0];
+
+        // 두번째 집 선택하는 경우
+        dp2[0] = 0;
+        dp2[1] = money[1];
+
+        for (int i = 2; i < len; i++) {
+            if (i != len - 1)
+                dp1[i] = Math.max(dp1[i - 2] + money[i], dp1[i - 1]);
+
+            dp2[i] = Math.max(dp2[i - 2] + money[i], dp2[i - 1]);
+        }
+        return Math.max(dp1[len - 2], dp2[len - 1]);
+    }
 }
