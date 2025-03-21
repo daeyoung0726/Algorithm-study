c334e8c27224590e3dffb41a3424dfe020596d40
DaeYoung0726 <pdy000726@naver.com>
2025-03-21 10:24:37 +0900

    programmers - 비밀 코드 해독

--- src/programmers/비밀_코드_해독.java
+++ src/programmers/비밀_코드_해독.java
@@ -1,2 +1,52 @@
-package programmers;public class 비밀_코드_해독 {
+package programmers;
+
+// dfs.
+
+import java.util.*;
+
+public class 비밀_코드_해독 {
+    private List<Integer> list;
+    private int answer = 0;
+    private int[][] q;
+    private int[] ans;
+
+    public int solution(int n, int[][] q, int[] ans) {
+        list = new ArrayList<>();
+        this.q = q;
+        this.ans = ans; 
+
+        dfs(n, 0, 1);
+        return answer;
+    }
+
+    private void dfs(int n, int count, int x) {
+        if (count == 5) {
+            if (check())
+                answer++;
+            return;
+        }
+
+        for (int i = x; i <= n; i++) {
+            list.add(i);
+            dfs(n, count + 1, i + 1);
+            list.remove((Integer) i);
+        }
+    }
+
+    private boolean check() {
+        for (int i = 0; i < q.length; i++) {
+            int count = 0;
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (q[i][j] == list.get(k)) {
+                        count++;
+                        continue;
+                    }
+                }
+            }
+            if (count != ans[i])
+                return false;
+        }
+        return true;
+    }
 }
