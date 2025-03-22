333458365b0ef374bc307e74d7e9633b083faa46
DaeYoung0726 <pdy000726@naver.com>
2025-03-22 21:32:42 +0900

    programmers - 삼각 달팽이

--- src/programmers/삼각_달팽이.java
+++ src/programmers/삼각_달팽이.java
@@ -1,2 +1,45 @@
-package programmers;public class 삼각_달팽이 {
+package programmers;
+
+public class 삼각_달팽이 {
+    public int[] solution(int n) {
+        int[][] arr = new int[n][n];
+
+        int x = 0;
+        int y = 0;
+        int num = 1;
+        arr[0][0] = num++;
+
+        while (true) {
+            boolean check = false;
+
+            while (x + 1 < n && arr[x + 1][y] == 0) {
+                arr[++x][y] = num++;
+                check = true;
+            }
+
+            if (!check)
+                break;
+
+            while (y + 1 < n && arr[x][y + 1] == 0) {
+                arr[x][++y] = num++;
+            }
+
+            while (x - 1 >= 0 && y - 1 >= 0 && arr[x - 1][y - 1] == 0) {
+                arr[--x][--y] = num++;
+            }
+
+        }
+
+        int[] answer = new int[num - 1];
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] != 0) {
+                    answer[idx++] = arr[i][j];
+                }
+            }
+        }
+
+        return answer;
+    }
 }
