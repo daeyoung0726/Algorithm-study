548e7bfd7e31ad448cf3c7a5ea387bf32e2a3744
DaeYoung0726 <pdy000726@naver.com>
2025-03-23 22:43:13 +0900

    programmers - 야근 지수

--- src/programmers/야근_지수.java
+++ src/programmers/야근_지수.java
@@ -1,2 +1,26 @@
-package programmers;public class 야근_지수 {
+package programmers;
+
+import java.util.*;
+
+public class 야근_지수 {
+    public long solution(int n, int[] works) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int work : works) {
+            pq.add(work);
+        }
+
+        while (n > 0 && !pq.isEmpty()) {
+            int num = pq.poll();
+            if (num != 0)
+                pq.add(num - 1);
+            n--;
+        }
+
+        long answer = 0;
+
+        while (!pq.isEmpty()) {
+            answer += (long) Math.pow(pq.poll(), 2);
+        }
+        return answer;
+    }
 }
